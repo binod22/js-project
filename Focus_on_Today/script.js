011ab8c363f5bcb395c42f44c0687595641d8b61
Binod <regbinod22@gmail.com>
2024-11-12 13:05:15 +0545

    Day-4 Implementing Local Storage and Finalizing the project with script

--- Focus_on_Today/script.js
+++ Focus_on_Today/script.js
@@ -4,7 +4,8 @@ const errorlabel = document.querySelector('.alert-label');
 const progressbar = document.querySelector('.progress-bar')
 const progressvalue = document.querySelector('.progress-value')
 
-console.log(checkBoxList);
+const allgoals = JSON.parse(localStorage.getItem('allgoals')) || {};
+let completedGoalCount=Object.values(allgoals).filter((goal) => goal.completed).length
 checkBoxList.forEach((box)=>{
     box.addEventListener('click',(e)=>{
         const allFieldsFilled = [...inputField].every((input)=>{
@@ -12,6 +13,14 @@ checkBoxList.forEach((box)=>{
         })
         if(allFieldsFilled){
         box.parentElement.classList.toggle('completed')
+        
+        inputID = box.nextElementSibling.id
+        allgoals[inputID].completed = !allgoals[inputID].completed;
+        completedGoalCount=Object.values(allgoals).filter((goal) => goal.completed).length
+        console.log(completedGoalCount) 
+        progressvalue.style.width = `${33.33*completedGoalCount}%`
+        progressvalue.innerHTML = `<span>${completedGoalCount}/3 Completed</span>`
+        localStorage.setItem('allgoals',JSON.stringify(allgoals))
         }
         else{
             progressbar.classList.add('show-error')
@@ -20,7 +29,21 @@ checkBoxList.forEach((box)=>{
 })
 
 inputField.forEach((input)=>{
+    input.value = allgoals[input.id].name
+    if(allgoals[input.id].completed){
+        input.parentElement.classList.add('completed')
+    }
     input.addEventListener('focus',(e)=>{
         progressbar.classList.remove('show-error')
+
+    })
+    input.addEventListener('input',(e)=>{
+        allgoals[input.id]= {
+            name : input.value,
+            completed: false
+        }
+        localStorage.setItem('allgoals',JSON.stringify(allgoals))
     })
 })
+
+
